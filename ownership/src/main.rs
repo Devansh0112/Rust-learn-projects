543d83cfad36b1b47270497773d13812e44d3a7c
Devansh <devansh0112@gmail.com>
2024-11-26 13:17:30 -0500

    references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,7 +1,21 @@
+// fn main() {
+//     let s1 = String::from("hello");
+
+//     let len = calculate_length(&s1);
+
+//     println!("The length of '{s1}' is {len}.");
+// }
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
+
 fn main() {
     let mut s = String::from("hello");
 
-    s.push_str(", world!");
+    change(&mut s);
+}
 
-    println!("{s}");
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
