3420f6ce18df82082a072ef645288e62fd2cb747
Devansh <devansh0112@gmail.com>
2024-11-27 08:53:34 -0500

    the slice

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -10,12 +10,37 @@
 //     s.len()
 // }
 
-fn main() {
-    let mut s = String::from("hello");
+// fn main() {
+//     let mut s = String::from("hello");
 
-    change(&mut s);
+//     change(&mut s);
+// }
+
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+fn main() {
+    let mut s = String::from("are you saying something?");
+
+    let word = first_word(&s); // word will get the value 5
+
+    s.clear(); // this empties the String, making it equal to ""
+
+    println!("{word}");
+
+    // word still has the value 5 here, but there's no more string that
+    // we could meaningfully use the value 5 with. word is now totally invalid!
 }
